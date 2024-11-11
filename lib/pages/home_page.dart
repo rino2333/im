7f620f6f0e6971608b915e1a25f020fe7227c5a6
hutao <983210535@qq.com>
2024-11-11 17:36:49 +0800

    upd

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,12 +12,14 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(44.0), 
         child: AppBar(
           title: Text('IM'),
           backgroundColor: Color(0xffeaeaea),
-        )),
+        )
+      ),
       body: ContactsPage(),
     );
   }
@@ -37,20 +39,18 @@ class _ContactsPageState extends State<ContactsPage> {
       return ListView(
         children: [
           for (var contact in contacts) 
-            ListTile(
-              title: Text(contact),
-              leading: Image.asset('assets/images/dandan.png', width: 78, height: 78,),
-              trailing: Icon(Icons.chevron_right),
-              onTap: () {
-              },
-            ),
-          for (var contact in contacts) 
+            // ListTile(
+            //   leading: ClipRRect(
+            //     borderRadius: BorderRadius.circular(10),
+            //     child: Image.asset('assets/images/dandan.png', width: 50, height: 50,),
+            //   ),
+            //   title: Text(contact, style: const TextStyle(fontWeight: FontWeight.bold),),
+            //   subtitle: Text('Some other info', style: const TextStyle(fontSize: 12),),
+            // ),
             Container(
-              alignment: Alignment.centerLeft,
-              // height: 68,
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+              height: 72,
+              padding: const EdgeInsets.only(left: 12,),
               color: Color(0xffffffff),
-              // color: Color.fromARGB(255, 47, 189, 104),
               child: Row(
                 children: [
                   ClipRRect(
@@ -65,18 +65,29 @@ class _ContactsPageState extends State<ContactsPage> {
                           bottom: BorderSide(
                             color: Colors.grey,
                             width: 0.3,
-                          )
+                          ),
                         )
                       ),
-                      child: Text(contact),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            contact, 
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 6,),
+                          Text(
+                            'Some other info',
+                            style: const TextStyle(fontSize: 12),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
-            // BigCard(contact: contact),
-          // Image(image: AssetImage('https://docs.flutter.cn/assets/images/cn/flutter-cn-logo.png')),
-          // Image.network('https://docs.flutter.cn/assets/images/cn/flutter-cn-logo.png')
         ],
       );
   }
